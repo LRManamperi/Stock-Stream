67e090e4fb9100a50e9d1ea5d386ed3d54ff2ab0
LaRasa99 <134576794+LRManamperi@users.noreply.github.com>
2025-03-19 14:27:10 +0530

    Theme created

---
+++ Frontend/frontend/src/theme.js
@@ -0,0 +1,11 @@
+// theme.js
+export const lightTheme = {
+    backgroundColor: '#fff',
+    color: '#000',
+  };
+  
+  export const darkTheme = {
+    backgroundColor: '#333',
+    color: '#fff',
+  };
+  
