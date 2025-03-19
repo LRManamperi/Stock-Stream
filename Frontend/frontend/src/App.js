cfa4a25cd357ca08de3b86bfda8bfefbf20dd515
LaRasa99 <134576794+LRManamperi@users.noreply.github.com>
2025-03-19 10:52:38 +0530

    Routes done

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apolloClient";
+import StockChart from "./components/StockChart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h1>StockStream 📈</h1>
+        <StockChart symbol="AAPL" />
+      </div>
+    </ApolloProvider>
   );
 }
 
